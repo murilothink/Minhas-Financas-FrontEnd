dd5cf24eb82d00910e2a9670eccac80bcac3151e
Murilo Henrique <34652210+murilothink@users.noreply.github.com>
2020-08-03 15:06:25 -0300

    Adicionando navbar

--- src/views/login.js
+++ src/views/login.js
@@ -15,49 +15,47 @@ class Login extends React.Component{
         console.log('senha: ', this.state.senha)
     }
 
+
     render(){
         return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6" style={{position: 'relative', left:'300px'}}>
-                        <div className="bs-docs-section">
-                            <Card title="Login">
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                        <div className="bs-component">
-                                            <fieldset>
-                                                <h1 style={{fontSize:17}}>Email: *</h1>
-                                                <formGroup htmlFor="exampleInputEmail1" label="Email:">
-                                                    <input type="email"
-                                                            value={this.state.email}
-                                                            onChange={e => this.setState({email: e.target.value})} 
-                                                            class="form-control" 
-                                                            id="exampleInputEmail1" 
-                                                            aria-describedby="emailHelp" 
-                                                            placeholder="Digite o Email"/>
-                                                </formGroup>
-                                                <br></br>
-                                                <h1 style={{fontSize:17}}>Senha: *</h1>
-                                                <formGroup label="Senha: *" htmlFor="exampleInputPassword1">
-                                                    <input type="password"
-                                                            value={this.state.senha}
-                                                            onChange={e => this.setState({senha: e.target.value})}  
-                                                            class="form-control" 
-                                                            id="exampleInputPassword1" 
-                                                            placeholder="Password"/>
-                                                </formGroup>
+            <div className="row">
+                <div className="col-md-6" style={{position: 'relative', left:'300px'}}>
+                    <div className="bs-docs-section">
+                        <Card title="Login">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                            <h1 style={{fontSize:17}}>Email: *</h1>
+                                            <formGroup htmlFor="exampleInputEmail1" label="Email:">
+                                                <input type="email"
+                                                        value={this.state.email}
+                                                        onChange={e => this.setState({email: e.target.value})} 
+                                                        class="form-control" 
+                                                        id="exampleInputEmail1" 
+                                                        aria-describedby="emailHelp" 
+                                                        placeholder="Digite o Email"/>
+                                            </formGroup>
+                                            <br></br>
+                                            <h1 style={{fontSize:17}}>Senha: *</h1>
+                                            <formGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                <input type="password"
+                                                        value={this.state.senha}
+                                                        onChange={e => this.setState({senha: e.target.value})}  
+                                                        class="form-control" 
+                                                        id="exampleInputPassword1" 
+                                                        placeholder="Password"/>
+                                            </formGroup>
                                                 <button onClick={this.entrar} className="btn btn-success" style={{margin: '2px'}}>Entrar</button>
                                                 <button className="btn btn-danger">Cadastrar</button>
-                                            </fieldset>
-                                        </div>
+                                        </fieldset>
                                     </div>
                                 </div>
-                            </Card>
-                        </div>
+                            </div>
+                        </Card>
                     </div>
                 </div>
             </div>
-
         )
     }
 }
